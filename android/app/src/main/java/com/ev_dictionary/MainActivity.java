1ce8b684eca849a17b8626b1c1cd8d3e30cc55d4
Khoa <nekennick@gmail.com>
2023-07-10 11:11:19 +0700

    V2

--- android/app/src/main/java/com/ev_dictionary/MainActivity.java
+++ android/app/src/main/java/com/ev_dictionary/MainActivity.java
@@ -1,6 +1,21 @@
 package com.ev_dictionary;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                }
+        );
+    }
+
+
 }
